e897b82783eaa7b9385fa0b368fbe15ba6249e32
quynhchi <doquynhchi1311@gmail.com>
2023-04-23 20:29:17 +0700

    update size of ClinicCard

--- screens/VeterinarianScreens/V_ListVetClinicScreen.js
+++ screens/VeterinarianScreens/V_ListVetClinicScreen.js
@@ -6,6 +6,7 @@ import {
   View,
   Image,
   ScrollView,
+  Dimensions,
 } from "react-native";
 import React, { Component } from "react";
 
@@ -43,19 +44,19 @@ export default function V_ListVetClinicScreen({ navigation }) {
 
   return (
     <View style={styles.wrapping}>
-      <View style={styles.header}>
+      <View style={[styles.header, styles.row]}>
         <TouchableOpacity style={styles.backBtn} onPress={() => navigation.goBack()}>
           <Image
             style={styles.backIcon}
             source={require("../../assets/icons/V_backIconMain.png")}
           ></Image>
         </TouchableOpacity>
-        <View style={styles.headerTitle}>
+        <View style={[styles.headerTitle, styles.row]}>
           <Text style={styles.headerText}>Đặt lịch hẹn</Text>
           <Image style={styles.headerImg} source={require('../../assets/icons/V_bookingVetHeader.png')}></Image>
         </View>
       </View>
-      <View style={styles.title}>
+      <View style={[styles.title, styles.row]}>
         <Image style={styles.titleImg} source={require('../../assets/icons/V_VetClinicLocation.png')}></Image>
         <Text style={styles.titleText}>Các phòng khám ở gần bạn</Text>
       </View>
@@ -68,8 +69,22 @@ export default function V_ListVetClinicScreen({ navigation }) {
 
 const ClinicCard = (prop) => {
   return (
-    <View style={styles.clinicCard}>
-      <View>
+    <View style={[styles.clinicCard, styles.row]}>
+      <View style={{
+        // add shadows for Android only
+        elevation: 6,
+
+        // add shadows for iOS only
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.5,
+
+        // fitting view with image
+        width: 68,
+        height: 68,
+        borderRadius: 40,
+        marginRight: 8,
+      }}>
         <Image style={styles.clinicImg} source={require('../../assets/images/V_clinicAvatar.png')}></Image>
       </View>
       {/* 
@@ -77,26 +92,24 @@ const ClinicCard = (prop) => {
         */}
       <View style={styles.clinicInfo}>
         <Text style={styles.name}>{prop.name}</Text>
-        <View style={styles.address}>
+        <View style={[styles.address, styles.row]}>
           <Image style={styles.addressIcon} source={require('../../assets/icons/V_clinic-location.png')}></Image>
           <Text style={styles.addressText}>{prop.address}</Text>
         </View>
-        <View style={styles.numOfFollowers}>
+        <View style={[styles.numOfFollowers, styles.row]}>
           <Image style={styles.followerIcon} source={require('../../assets/icons/V_followers.png')}></Image>
           <Text style={styles.followerNumber}>{prop.followers}</Text>
-          <Text> Người theo dõi</Text>
+          <Text style={styles.followerText}> Người theo dõi</Text>
         </View>
         <View style={styles.row}>
           {/* chat with clinic button */}
           <TouchableOpacity style={[styles.chatBtn, styles.row]}>
             <Image style={styles.iconBtn} source={require('../../assets/icons/V_message-icon.png')}></Image>
-            <Text style={styles.textChatBtn}>Hỏi đáp</Text>
           </TouchableOpacity>
           {/* booking vet button */}
-          <TouchableOpacity 
+          <TouchableOpacity
             style={[styles.bookingBtn, styles.row]}
             onPress={() => navigation.navigate("V_BookingVet")}>
-            <Image style={styles.iconBtn} source={require('../../assets/icons/V_booking-icon.png')}></Image>
             <Text style={styles.textBookingBtn}>Đặt lịch khám</Text>
           </TouchableOpacity>
         </View>
@@ -108,132 +121,155 @@ const ClinicCard = (prop) => {
 const styles = StyleSheet.create({
   wrapping: {
     height: '100%',
-    backgroundColor: '#FFF6F6',
+    backgroundColor: '#FFFFFF',
+  },
+  row: {
+    flexDirection: 'row',
   },
   header: {
     width: '100%',
-    flexDirection: 'row',
-    alignContent: 'space-between',
+    marginTop: '1%',
   },
   backBtn: {
-    left: 8,
     position: 'absolute',
+    left: 8,
     resizeMode: 'contain',
     marginTop: 32,
     marginLeft: 12,
   },
   title: {
-    flexDirection: 'row',
-    marginBottom: 8,
+    backgroundColor: '#FFF6F6',
+    paddingVertical: 12,
+  },
+  titleImg: {
+    width: 28,
+    height: 28,
+    marginLeft: 24,
+    marginRight: 4,
   },
   titleText: {
-    fontSize: 16,
-    color: '#FFFFFF',
+    width: '72%',
+    fontSize: 15,
     fontFamily: 'lexend-semibold',
-    padding: 12,
-    borderRadius: 16,
+    color: '#FFFFFF',
     backgroundColor: '#F5817E',
+    borderRadius: 12,
+    paddingVertical: 4,
+    paddingLeft: 8,
   },
   headerTitle: {
     width: '100%',
-    flexDirection: 'row',
     justifyContent: 'center',
     marginTop: 8,
   },
   headerText: {
     color: '#A51A29',
-    fontSize: 18,
+    fontSize: 16,
     verticalAlign: 'middle',
     fontFamily: 'lexend-bold',
   },
   headerImg: {
-    width: '8%',
+    width: '6%',
     marginLeft: 6,
     resizeMode: 'contain',
   },
+  listClinicCard: {
+    marginTop: 8,
+    marginBottom: Dimensions.get("window").height * 0.1,
+  },
   clinicCard: {
-    width: '88%',
+    width: '84%',
     alignSelf: 'center',
-    flexDirection: 'row',
     backgroundColor: '#FFFFFF',
     borderColor: '#F5817E',
     borderWidth: 1,
-    borderRadius: 16,
-    padding: 12,
-    marginBottom: 16,
+    borderRadius: 8,
+    marginBottom: 12,
+    paddingTop: 6,
+    paddingBottom: 12,
+    paddingHorizontal: 8,
+
+    // add shadows for Android only
+    elevation: 8,
+    // add shadows for iOS only
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
   },
   clinicImg: {
-    width: 100,
-    height: 100,
+    width: 68,
+    height: 68,
     borderRadius: 64,
     borderWidth: 2,
-    borderColor: '#DCDCDC',
-    marginRight: 8,
+    borderColor: '#FFFFFF',
   },
   name: {
     color: '#39A3C0',
-    fontSize: 18,
+    fontSize: 16,
     fontFamily: 'lexend-regular',
+    marginBottom: 4,
   },
   address: {
     width: '80%',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
   },
   addressIcon: {
-    width: 16,
+    width: 18,
     height: 18,
     marginTop: 4,
+    resizeMode: 'contain',
   },
   addressText: {
     fontSize: 14,
-    fontFamily: 'lexend-light',
-    color: '#615D5D',
+    fontFamily: 'lexend-regular',
+    color: '#3b3b3b',
     marginLeft: 6,
   },
   numOfFollowers: {
     width: '80%',
-    flexDirection: 'row',
     alignItems: 'flex-end',
     marginBottom: 12,
   },
   followerIcon: {
     width: 16,
-    height: 18,
+    height: 16,
     marginTop: 4,
+    marginBottom: 3,
+  },
+  followerNumber: {
+    fontSize: 14,
+    fontFamily: 'lexend-regular',
+    color: '#3b3b3b',
+    marginLeft: 6,
+    marginBottom: 0,
   },
   followerText: {
-    fontSize: 14,
-    fontFamily: 'lexend-light',
-    color: '#615D5D',
-    marginLeft: 6,
-  },
-  row: {
-    flexDirection: 'row',
+    color: '#3b3b3b',
+    marginBottom: 0,
   },
   chatBtn: {
-    backgroundColor: '#FFFFFF',
     borderColor: '#A51A29',
     borderWidth: 1,
     padding: 8,
-    borderRadius: 16,
+    borderRadius: 4,
     marginRight: 8,
   },
   bookingBtn: {
+    width: '68%',
+    justifyContent: 'center',
     backgroundColor: '#A51A29',
-    padding: 8,
-    borderRadius: 16,
+    borderColor: '#E02D33',
+    borderWidth: 1,
+    paddingVertical: 8,
+    borderRadius: 8,
   },
   iconBtn: {
-    width: 16,
-    height: 16,
-  },
-  textChatBtn: {
-    fontSize: 12,
+    width: 24,
+    height: 18,
   },
   textBookingBtn: {
-    fontSize: 12,
+    fontSize: 13,
     fontFamily: 'lexend-semibold',
     color: '#FFFFFF',
+    marginLeft: 6,
   }
 });
